3cb4a9658d97b0b0083443eba7f318775ea1a1c3
coretx09 <ngampiosauvet@gmail.com>
2022-02-26 10:05:42 +0300

    finish

---
+++ app/src/main/java/com/example/android/unscramble/documentation/liveData.md
@@ -0,0 +1,67 @@
+# LIVEDATA
+La [LiveData] classe fait également partie des composants d'architecture Android et est une classe de support de données qui peut être observée.
+LiveData est une classe de détenteurs de données observables qui tient compte du lifeCycle.
+
+[LiveData] contient des données ; LiveData est un wrapper qui peut être utilisé avec n'importe quel type de données.
+
+[LiveData] est observable, ce qui signifie qu'un observateur est averti lorsque les données détenues par l' LiveData objet changent.
+
+[LiveData] est conscient du lifecycle. Lorsque vous attachez un observateur au LiveData,
+l' [observer] est associé à un [LifecycleOwner](généralement une activité ou un fragment). 
+
+# MutableLiveData
+c'est la version modifiable de LiveData, c'est-à-dire que la valeur des données qui y sont stockées peut être modifiée.
+
+Le type de variable [val] car la valeur de l' objet [LiveData/ MutableLiveData] restera la même et 
+seules les données stockées dans l'objet changeront.
+
+# Value LiveData object
+Pour accéder aux données d'un LiveData objet, utilisez la [value] propriété
+
+# Attach observer to the LiveData object .observe()
+[observer] que vous allez ajouter observe les modifications apportées aux données de l'application currentScrambledWord.
+Ainsi, [observer] dans le GameFragment ne sera averti que lorsque le GameFragment est dans [STARTED] ou [RESUMED] indique.
+
+  ## observe() method 
+[viewLifecycleOwner] représente le lifecycle de la view du fragment .
+Ce paramètre permet à la LiveData d'être consciente du GameFragment lifecycle et de notifier 
+l' observer uniquement lorsque la GameFragment est dans des états actifs ( STARTED ou RESUMED).
+
+# Use LiveData with data binding
+L'application écoute les modifications de données dans le code. De même, les applications peuvent écouter
+les modifications de données à partir de la mise en page.
+ 
+[View Binding] -  est une fonctionnalité qui vous permet d'accéder plus facilement aux [views] dans le code.
+Une instance d'une binding class contient des références directes à toutes les views qui ont un ID dans la mise en page correspondante.
+C'est une liaison à sens unique.
+vous ne pouvez pas référencer les données d'application dans les views (fichiers de mise en page). Cela peut être accompli en utilisant [data binding]
+
+[data binding] - fait également partie de la bibliothèque Android Jetpack
+Lie les composants d'UI de vos mises en page aux sources de données de votre application à l'aide d'un format déclaratif,
+In simpler terms Data binding is binding data (from code) to views + view binding (binding views to code)
+
+[1.] buildFeatures {
+   dataBinding = true
+   }
+[2.] plugins {
+id 'com.android.application'
+id 'kotlin-android'
+id 'kotlin-kapt' on doit applique ce ci
+}
+
+Convert layout file to data binding layout
+
+[Add data binding variables]-
+
+# Use binding expressions
+sont écrites dans la mise en page dans les [propriétés d'attribut]
+Les propriétés de mise en page sont déclarées en haut du fichier de mise en page de data binding, via la [<variable>] balise
+Lorsque l'une des variables dépendantes change, la 'DB Library' exécute vos expressions de liaison (et met ainsi à jour les views). 
+
+[Syntaxe des binding expressions ]
+Les binding expressions  commencent par un [ @ ]symbole et sont entourées d'accolades [ {} ]. 
+ #Resources in data binding expressions:
+  [1. android:padding="@{@dimen/largePadding}" ]-  padding attribut est affecté d'une valeur à largePadding a partir du dimen.xml
+  [2. android:text="@{@string/example_resource(user.lastName)}"]
+
+
--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -1,26 +1,53 @@
 package com.example.android.unscramble.ui.game
 
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.TtsSpan
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
 class GameViewModel: ViewModel() {
 
-    // SCORE GAME
-    private var _score = 0
-    val score: Int get() = _score
+    /* PROPERTY */
 
-    // NOMBRE DE MOTS
-    private var _currentWordCount = 0
-    val currentWordCount: Int
+    // SCORE GAME
+    private val _score = MutableLiveData(0)
+    val score: LiveData<Int> get() = _score
+
+    // NOMBRE DE MOTS ACTUEL
+    private val _currentWordCount = MutableLiveData(0)
+    val currentWordCount: LiveData<Int>
         get() = _currentWordCount
 
-    private lateinit var _currentScrambledWord : String
-    val currentScrambledWord: String
-        get() = _currentScrambledWord
+    // MOTS BROUILLE ACTUEL
+    private  val _currentScrambledWord = MutableLiveData<String>()
+    val currentScrambledWord: LiveData<Spannable> = Transformations.map(_currentScrambledWord) {
+        if (it == null) {
+            SpannableString("")
+        } else {
+            val scrambledWord = it.toString()
+            val spannable: Spannable = SpannableString(scrambledWord)
+            spannable.setSpan(
+                TtsSpan.VerbatimBuilder(scrambledWord).build(),
+                0,
+                scrambledWord.length,
+                Spannable.SPAN_INCLUSIVE_INCLUSIVE
+            )
+            spannable
+        }
+    }
 
-    // liste de mots
+    // LIST DES MOTS
     private var wordsList =  mutableListOf<String>()
-    // mots a dechiffrer
+
+    // MOTS ACTUEL
     private lateinit var currentWord: String
+
+
+
+    /* METHODS */
 
     private fun getNextWord() {
         currentWord = allWordsList.random()
@@ -31,12 +58,13 @@ class GameViewModel: ViewModel() {
         while(String(tempWord).equals(currentWord, false)) {
             tempWord.shuffle()
         }
-
         // pour vérifier si un mot a déjà été utilisé
         if (wordsList.contains(currentWord)) getNextWord()
         else {
-            _currentScrambledWord = String(tempWord)
-            ++_currentWordCount
+            // OBJECT LIVEDATA: GET VALUE
+            _currentScrambledWord.value = String(tempWord)
+            // inc()fonction Kotlin pour incrémenter la valeur avec une sécurité nulle.
+            _currentWordCount.value = (_currentWordCount.value)?.inc()
             wordsList.add(currentWord)
         }
     }
@@ -46,17 +74,20 @@ class GameViewModel: ViewModel() {
      * Updates the next word.
      */
     fun nextWord(): Boolean {
-        return if (currentWordCount < MAX_NO_OF_WORDS) {
+        return if (currentWordCount.value!! < MAX_NO_OF_WORDS) {
             getNextWord()
             true
         } else false
     }
 
-    //méthode d'assistance pour valider le mot du joueur
+    /* Méthodes d'assistance pour valider le mot du joueur  ajoute +20 a chaque mots correct*/
+    // LE SCORE ++
     private fun increaseScore() {
-        _score += SCORE_INCREASE
+        // plus()fonction  effectue l'addition avec une sécurité nulle.
+        _score.value = (_score.value)?.plus(SCORE_INCREASE)
     }
 
+    // RETURN LE SCORE
     fun isUserWordCorrect(playerWord: String): Boolean {
         if (playerWord.equals(currentWord, true)) {
             increaseScore()
@@ -66,11 +97,11 @@ class GameViewModel: ViewModel() {
     }
 
     /*
-* Re-initializes the game data to restart the game.
-*/
+     * Re-initializes the game data to restart the game.
+     */
     fun reinitializeData() {
-        _score = 0
-        _currentWordCount = 0
+        _score.value = 0
+        _currentWordCount.value = 0
         wordsList.clear()
         getNextWord()
     }
